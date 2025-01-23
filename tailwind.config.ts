4a609a8ee4d10cfdb6afb6e42ebd9016989a6394
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-01-23 14:58:25 +0600

    feat: initial first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+				playfair: ["Playfair Display", "serif"],
+			},
+			fontWeight: {
+				bolder: "900",
+			},
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+        primary: '',
+        secondary: '',
+      }
+		},
+	},
+	plugins: [],
 } satisfies Config;
