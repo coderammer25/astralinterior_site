5d49781518ae5ce48a234ff929cbe7ba3be39e24
rihan <rihan@rihans-MacBook-Pro.local>
2025-02-19 11:13:52 +0600

    Admin Login fix

--- src/components/home-page/banner.tsx
+++ src/components/home-page/banner.tsx
@@ -56,12 +56,40 @@ const Banner = () => {
 					Crafting <br /> luxury <span className="font-playfair">spaces</span>
 				</h1>
 				<div className="relative z-0">
+				<Image
+						src="https://cdn.prod.website-files.com/6713afdf3556af8f35af46f9/6713b50244f3d6b890df9977_Futuristic%20Capsule%20Chair-p-500.webp"
+						alt="Chair Image"
+						width={800}
+						height={800}
+						className="w-[22rem] h-[28rem] mx-auto absolute top-32 left-[300px] -translate-x-1/2 z-10 -rotate-45 "
+					/>
 					<Image
 						src="https://cdn.prod.website-files.com/6713afdf3556af8f35af46f9/6713b50244f3d6b890df9977_Futuristic%20Capsule%20Chair-p-500.webp"
 						alt="Chair Image"
 						width={800}
 						height={800}
-						className="w-[22rem] h-[28rem] mx-auto absolute top-0 left-1/2 -translate-x-1/2"
+						className="w-[22rem] h-[28rem] mx-auto absolute top-10 left-1/3 -translate-x-1/2 z-10 -rotate-12 "
+					/>
+					<Image
+						src="https://cdn.prod.website-files.com/6713afdf3556af8f35af46f9/6713b50244f3d6b890df9977_Futuristic%20Capsule%20Chair-p-500.webp"
+						alt="Chair Image"
+						width={800}
+						height={800}
+						className="w-[22rem] h-[28rem] mx-auto absolute -top-6 left-1/2 -translate-x-1/2 z-50"
+					/>
+					<Image
+						src="https://cdn.prod.website-files.com/6713afdf3556af8f35af46f9/6713b50244f3d6b890df9977_Futuristic%20Capsule%20Chair-p-500.webp"
+						alt="Chair Image"
+						width={800}
+						height={800}
+						className="w-[22rem] h-[28rem] mx-auto absolute top-[35px] left-2/3 -translate-x-[170px] rotate-12 z-10"
+					/>
+					<Image
+						src="https://cdn.prod.website-files.com/6713afdf3556af8f35af46f9/6713b50244f3d6b890df9977_Futuristic%20Capsule%20Chair-p-500.webp"
+						alt="Chair Image"
+						width={800}
+						height={800}
+						className="w-[22rem] h-[28rem] mx-auto absolute top-32 left-2/3 -translate-x-[50px] z-0 rotate-45"
 					/>
 				</div>
 				<div className="relative top-[500px] lg:top-96 flex flex-col gap-4 lg:flex-row lg:gap-0 items-center justify-between">
