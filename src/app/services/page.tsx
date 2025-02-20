7ab31b7341c4809481ff1b8dbd8be9e2dde8d557
rihan <rihan@rihans-MacBook-Pro.local>
2025-02-20 17:31:17 +0600

    fix: meta data fix in components

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,9 +2,9 @@ import Services from "@/components/services";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Our Services - Interior Design & Custom Furniture | Astra",
+	title: "Our Services - Interior Design & Custom Furniture | Astral",
 	description:
-		"Explore Astra Interior & Furniture's premium interior design and custom furniture services. Transform your space with expert craftsmanship.",
+		"Explore Astral Interior & Furniture's premium interior design and custom furniture services. Transform your space with expert craftsmanship.",
 };
 
 export default function ServicesPage() {
