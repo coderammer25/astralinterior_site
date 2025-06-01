9fb91bb40c917873b974212fb190457a9733e199
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-06-01 14:13:58 +0600

    feat: add logo section

---
+++ src/data/logos.ts
@@ -0,0 +1,76 @@
+import logo1 from "../../public/logos/logo1.png"
+import logo2 from "../../public/logos/logo2.jpg"
+import logo3 from "../../public/logos/logo3.jpg"
+import logo4 from "../../public/logos/logo4.jpg"
+import logo5 from "../../public/logos/logo5.jpg"
+import logo6 from "../../public/logos/logo6.jpg"
+import logo7 from "../../public/logos/logo7.jpg"
+import logo8 from "../../public/logos/logo8.jpg"
+import logo9 from "../../public/logos/logo9.jpg"
+import logo10 from "../../public/logos/logo10.jpg"
+import logo11 from "../../public/logos/logo11.jpg"
+import logo212 from "../../public/logos/logo212.jpg"
+
+
+export const logos = [
+  {
+    id: 'logo-1',
+    title: "Zaman Group",
+    src: logo1,
+  },
+  {
+    id: 'logo-2',
+    title: "X Index",
+    src: logo2,
+  },
+  {
+    id: 'logo-3',
+    title: "Platinum Group",
+    src: logo3,
+  },
+  {
+    id: 'logo-4',
+    title: "KPL Group",
+    src: logo4,
+  },
+  {
+    id: 'logo-5',
+    title: "Best Electronics",
+    src: logo5,
+  },
+  {
+    id: 'logo-6',
+    title: "SB Group",
+    src: logo6,
+  },
+  {
+    id: 'logo-7',
+    title: "KNF Group",
+    src: logo7,
+  },
+  {
+    id: 'logo-8',
+    title: "Evergreen Group",
+    src: logo8,
+  },
+  {
+    id: 'logo-9',
+    title: "RPHL Group",
+    src: logo9,
+  },
+  {
+    id: 'logo-10',
+    title: "Royal Green",
+    src: logo10,
+  },
+  {
+    id: 'logo-11',
+    title: "Cotton Valley",
+    src: logo11,
+  },
+  {
+    id: 'logo-12',
+    title: "ACNABIN Chartered Accountants",
+    src: logo212 ,
+  },
+]
