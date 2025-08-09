a77bf683351528e44e99906154a6c876c58057e4
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-08-09 15:40:02 +0600

    form system done (EmailJs) ,The site is online

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "standalone",
+	output: "export",
+	trailingSlash: true,
 	images: {
+		unoptimized: true, // disable optimization for static export
 		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cdn.prod.website-files.com",
-			},
-			{
-				protocol: "https",
-				hostname: "assets.aceternity.com",
-			},
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-			},
-			{
-				protocol: "https",
-				hostname: "plus.unsplash.com",
-			},
-			{
-				protocol: "https",
-				hostname: "res.cloudinary.com",
-			},
+			{ protocol: "https", hostname: "cdn.prod.website-files.com" },
+			{ protocol: "https", hostname: "assets.aceternity.com" },
+			{ protocol: "https", hostname: "images.unsplash.com" },
+			{ protocol: "https", hostname: "plus.unsplash.com" },
+			{ protocol: "https", hostname: "res.cloudinary.com" },
 		],
 	},
 };
